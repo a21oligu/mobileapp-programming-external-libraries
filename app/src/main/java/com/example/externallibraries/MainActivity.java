fd398b9aecedafabf5e4090c564dd06f745e89b1
Oliver Gustafsson <oliver.gustafsson22@gmail.com>
2022-05-19 13:41:48 +0200

    now using Glide to load image into ImageView

--- app/src/main/java/com/example/externallibraries/MainActivity.java
+++ app/src/main/java/com/example/externallibraries/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.externallibraries;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
+
+    final private String IMAGE_URL = "https://images.unsplash.com/photo-1562102520-45f4dc579cf3?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2835";
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.main_image);
+        Glide.with(this).load(IMAGE_URL).into(imageView);
     }
 }
